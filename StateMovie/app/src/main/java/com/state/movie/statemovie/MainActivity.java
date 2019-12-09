c4416bbd867a1b80e8fe711353e12ab7c285195f
Adelcelevator <57175402+Adelcelevator@users.noreply.github.com>
2019-12-09 16:51:32 -0500

    Ya empece con el proyecto en el android primero voy a acabar el de netbeans pero acola con la presentacion

---
+++ StateMovie/app/src/main/java/com/state/movie/statemovie/MainActivity.java
@@ -0,0 +1,17 @@
+package com.state.movie.statemovie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActionBar;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActionBar ca=getActionBar();
+        ca.hide();
+    }
+}
