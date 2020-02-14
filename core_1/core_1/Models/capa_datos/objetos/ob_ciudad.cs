43f09f1b71e2a0e7c3839992585174324303218d
Adelcelevator <57175402+Adelcelevator@users.noreply.github.com>
2020-02-14 16:21:36 -0500

    Trabajos de java Web y recuperacion del core

---
+++ core_1/core_1/Models/capa_datos/objetos/ob_ciudad.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace core_1.Models.capa_datos.objetos
+{
+    public class ob_ciudad
+    {
+        public Int16 ciu_id { get; set; }
+        public string ciu_nom { get; set; }
+        public string ciu_est { get; set; }
+    }
+}
---
+++ core_1/core_1/Models/capa_datos/objetos/ob_sector.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace core_1.Models.capa_datos.objetos
+{
+    public class ob_sector
+    {
+        public Int16 sec_id { get; set; }
+        public Int16 ciu_id { get; set; }
+        public string sec_nom { get; set; }
+        public string sec_est { get; set; }
+    }
+}
