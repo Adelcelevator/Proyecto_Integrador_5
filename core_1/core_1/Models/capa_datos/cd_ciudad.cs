da2c608671632757b55914b5a585ca63f5dd2d72
Adelcelevator <57175402+Adelcelevator@users.noreply.github.com>
2020-02-14 16:51:00 -0500

    Trabajos de java Web y recuperacion del core

--- core_1/core_1/Models/capa_datos/cd_ciudad.cs
+++ core_1/core_1/Models/capa_datos/cd_ciudad.cs
@@ -1,11 +1,34 @@
-﻿using System;
+﻿using core_1.Models.capa_datos.objetos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace core_1.Models.capa_datos
 {
     public class cd_ciudad
     {
+        cd_conexion conexion = new cd_conexion();
+        ob_ciudad obci = new ob_ciudad();
+        SqlCommand comando = new SqlCommand();
+        SqlDataReader leer;
+
+
+        public DataTable todas()
+        {
+            DataTable tabla1 = new DataTable();
+            comando.Connection = conexion.abrir_conexion();
+            comando.CommandText = "mostrar_ciudades";
+            comando.CommandType = CommandType.StoredProcedure;
+            leer = comando.ExecuteReader();
+            tabla1.Load(leer);
+            comando.Parameters.Clear();
+            conexion.cerrar_conexion();
+            return tabla1;
+        }
+
+
     }
 }
---
+++ core_1/core_1/Models/capa_negocio/cn_ciudad.cs
@@ -0,0 +1,39 @@
+﻿using core_1.Models.capa_datos;
+using core_1.Models.capa_datos.objetos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+
+namespace core_1.Models.capa_negocio
+{
+    public class cn_ciudad
+    {
+        List<ob_ciudad> lis = new List<ob_ciudad>();
+        ob_ciudad obci = new ob_ciudad();
+        cd_ciudad cdci = new cd_ciudad();
+
+        public List<ob_ciudad> tod()
+        {
+            lis.Clear();
+            DataTable tabla = cdci.todas();
+            if (tabla.Rows.Count != 0)
+            {
+                foreach(DataRow dr in tabla.Rows)
+                {
+                    ob_ciudad nuevo = new ob_ciudad();
+                    nuevo.ciu_id = Convert.ToInt16(dr["ciu_id"].ToString());
+                    nuevo.ciu_nom = dr["ciu_nom"].ToString();
+                    nuevo.ciu_est = dr["ciu_est"].ToString();
+                    lis.Add(nuevo);
+                }
+                return lis;
+            }
+            else
+            {
+                return lis;
+            }
+        }
+    }
+}
